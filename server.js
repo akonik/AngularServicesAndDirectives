9ac3a638df5776699125d93a5d3038c50ec1944f
AlexKonik <alex.konik@softum.net>
2019-09-04 19:09:19 +0300

    server.js moved to root folder.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/AngularMaterials'));
+app.use(express.static('./dist/AngularServicesAndDirectives'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/AngularMaterials/index.html'));
+res.sendFile(path.join(__dirname,'/dist/AngularServicesAndDirectives/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
